2dd3014481be17bf6aa11b51dcb4d40eba293b0d
Ethan Conneely <econn50@outlook.com>
2025-02-03 03:20:58 +0000

    Remove notebook

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -15,33 +15,14 @@ import {
 	type ServerOptions,
 } from "vscode-languageclient/node";
 import { HurlEditorProvider } from "./hurlEditor";
-import {
-	parseMarkdown,
-	writeCellsToMarkdown,
-	type RawNotebookCell,
-} from "./notebook/markdownParser";
+
 
 let client: LanguageClient;
 
-const providerOptions = {
-	transientMetadata: {
-		runnable: true,
-		editable: true,
-		custom: true,
-	},
-	transientOutputs: true,
-};
 
 export function activate(context: ExtensionContext) {
 	context.subscriptions.push(HurlEditorProvider.register(context));
 
-	context.subscriptions.push(
-		workspace.registerNotebookSerializer(
-			"hurl-notebook-renderer",
-			new MarkdownProvider(),
-			providerOptions,
-		),
-	);
 
 	const serverModule = context.asAbsolutePath(
 		path.join("server", "out", "server.js"),
@@ -79,45 +60,4 @@ export function deactivate(): Promise<void> | undefined {
 		return undefined;
 	}
 	return client.stop();
-}
-
-class MarkdownProvider implements NotebookSerializer {
-	private readonly decoder = new TextDecoder();
-	private readonly encoder = new TextEncoder();
-
-	deserializeNotebook(
-		data: Uint8Array,
-		_token: CancellationToken,
-	): NotebookData | Thenable<NotebookData> {
-		const content = this.decoder.decode(data);
-
-		const cellRawData = parseMarkdown(content);
-		const cells = cellRawData.map(rawToNotebookCellData);
-
-		return {
-			cells,
-		};
-	}
-
-	serializeNotebook(
-		data: NotebookData,
-		_token: CancellationToken,
-	): Uint8Array | Thenable<Uint8Array> {
-		const stringOutput = writeCellsToMarkdown(data.cells);
-		return this.encoder.encode(stringOutput);
-	}
-}
-
-export function rawToNotebookCellData(data: RawNotebookCell): NotebookCellData {
-	return {
-		kind: data.kind,
-		languageId: data.language,
-		metadata: {
-			leadingWhitespace: data.leadingWhitespace,
-			trailingWhitespace: data.trailingWhitespace,
-			indentation: data.indentation,
-		},
-		outputs: [],
-		value: data.content,
-	};
 }
